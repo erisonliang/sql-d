4cb88546f9ae596075776063a21f529021bfeb29
RealOrko <realorko@nowhere.com>
2019-03-10 15:22:10 +0000

    Got install script working for linux

--- install.sh
+++ install.sh
@@ -14,4 +14,7 @@ mkdir ~/.sql-d/
 cp -R ./build/sql-d.start.linux-x64/contentFiles/any/any/sql-d.start/linux-x64/* ~/.sql-d/
 chmod +x ~/.sql-d/SqlD.Start.linux-x64
 sudo ln -fs ~/.sql-d/SqlD.Start.linux-x64 /usr/bin/sql-d
-xdg-open http://localhost:5000/swagger
+
+echo "[SQL-D]: Please type 'sql-d -w' to launch and browse to http://localhost:5000/swagger"
+echo "[SQL-D]: For more sql-d options, please type 'sql-d' and hit enter"
+echo "[SQL-D]: Example ~> sql-d -n newservice1 -s "localhost:9000" -d "newservice1.db" -r "localhost:9000" -w"
