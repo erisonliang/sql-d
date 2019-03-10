36f257f8d02c9ca7a988824c78716e98244f0bc3
RealOrko <RealOrko@nowhere.com>
2019-03-10 20:26:12 +0000

    Adding more help to bash scripts

--- docker.sh
+++ docker.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+echo "[SQL-D]: Please make sure you have run:"
+echo "[SQL-D]:  - docker-machine create father"
+echo "[SQL-D]:  - eval $(docker-machine env father)"
+
 docker build -t realorko/sql-d .
 docker run -d -p 5000:5000 -t realorko/sql-d
 docker-machine ls
 docker-machine start father
 eval $(docker-machine env father)
-docker run -i -t realorko/sql-d:latest -p 5000:5000
+docker run -t realorko/sql-d:latest -p 5000:5000
+
+echo "[SQL-D]: This will try and start a new sql-d.start.linux-x64 instance"
