155129f1de8ac9d891d45d7f12c6708c9df7bec2
RealOrko <45273739+RealOrko@users.noreply.github.com>
2019-03-15 00:18:24 +0000

    Nuget/sql.ui (#4)

--- publish.bat
+++ publish.bat
@@ -30,15 +30,15 @@ dotnet pack %StartWinX64ProjectPath% -o ../../build --include-symbols || EXIT /B
 
 SET UILinuxX64ProjectPath=./src/sql-d.ui.linux-x64/SqlD.UI.linux-x64.csproj
 dotnet publish %UILinuxX64ProjectPath% -r linux-x64 --self-contained
-xcopy /S /Q src\sql-d.ui.linux-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.linux-x64\bin\Release\netcoreapp2.2\linux-x64\publish\Views\
+xcopy /E /F /Y /S src\sql-d.ui.linux-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.linux-x64\bin\Release\netcoreapp2.2\linux-x64\publish\Views\
 dotnet pack %UILinuxX64ProjectPath% -o ../../build --include-symbols
 
 SET UIOsxX64ProjectPath=./src/sql-d.ui.osx-x64/SqlD.UI.osx-x64.csproj
 dotnet publish %UIOsxX64ProjectPath% -r osx-x64 --self-contained || EXIT /B 1
-xcopy /S /Q src\sql-d.ui.osx-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.osx-x64\bin\Release\netcoreapp2.2\osx-x64\publish\Views\
+xcopy /E /F /Y /S src\sql-d.ui.osx-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.osx-x64\bin\Release\netcoreapp2.2\osx-x64\publish\Views\
 dotnet pack %UIOsxX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
 
 SET UIWinX64ProjectPath=./src/sql-d.ui.win-x64/SqlD.UI.win-x64.csproj
 dotnet publish %UIWinX64ProjectPath% -r win-x64 --self-contained || EXIT /B 1
-xcopy /S /Q src\sql-d.ui.win-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.win-x64\bin\Release\netcoreapp2.2\win-x64\publish\Views\
+xcopy /E /F /Y /S src\sql-d.ui.win-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.win-x64\bin\Release\netcoreapp2.2\win-x64\publish\Views\
 dotnet pack %UIWinX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
