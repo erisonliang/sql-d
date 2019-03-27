2a95cc41cf553541969c48ce670e20c84681dcee
RealOrko <realorko@nowhere.com>
2019-03-27 20:31:13 +0000

    Porting build to linux-x64: Got publish script working.

--- publish.sh
+++ publish.sh
@@ -25,31 +25,31 @@ dotnet build $LibProjectPath
 dotnet pack $LibProjectPath -o ../../build
 	
 export StartLinuxX64ProjectPath=./src/sql-d.start.linux-x64/SqlD.Start.linux-x64.csproj
-dotnet publish $StartLinuxX64ProjectPath -r linux-x64 -f $TargetFramework --self-contained
-dotnet pack $StartLinuxX64ProjectPath -o ../../build --include-symbols
+dotnet publish $StartLinuxX64ProjectPath -c $Configuration -r linux-x64 -f $TargetFramework --self-contained
+dotnet pack $StartLinuxX64ProjectPath -c $Configuration -o ../../build --include-symbols
 	
 export StartOsxX64ProjectPath=./src/sql-d.start.osx-x64/SqlD.Start.osx-x64.csproj
-dotnet publish $StartOsxX64ProjectPath -r osx-x64 -f $TargetFramework --self-contained
-dotnet pack $StartOsxX64ProjectPath -o ../../build --include-symbols
+dotnet publish $StartOsxX64ProjectPath -c $Configuration -r osx-x64 -f $TargetFramework --self-contained
+dotnet pack $StartOsxX64ProjectPath -c $Configuration -o ../../build --include-symbols
 
 export StartWinX64ProjectPath=./src/sql-d.start.win-x64/SqlD.Start.win-x64.csproj
-dotnet publish $StartWinX64ProjectPath -r win-x64 -f $TargetFramework --self-contained
-dotnet pack $StartWinX64ProjectPath -o ../../build --include-symbols
+dotnet publish $StartWinX64ProjectPath -c $Configuration -r win-x64 -f $TargetFramework --self-contained
+dotnet pack $StartWinX64ProjectPath -c $Configuration -o ../../build --include-symbols
 
 export UIProjectPath=./src/sql-d.ui/SqlD.UI.csproj
 dotnet build $UIProjectPath
 	
 export UILinuxX64ProjectPath=./src/sql-d.ui.linux-x64/SqlD.UI.linux-x64.csproj
-dotnet publish $UILinuxX64ProjectPath -r linux-x64 --self-contained
-cp -rf src\sql-d.ui.linux-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.linux-x64\bin\Release\netcoreapp2.2\linux-x64\publish\Views\
-dotnet pack $UILinuxX64ProjectPath -o ../../build --include-symbols
+dotnet publish $UILinuxX64ProjectPath -c $Configuration -r linux-x64 -f $TargetFramework --self-contained
+cp -rf "src/sql-d.ui.linux-x64/bin/Release/${TargetFramework}/linux-x64/Views" "src/sql-d.ui.linux-x64/bin/Release/${TargetFramework}/linux-x64/publish/Views/"
+dotnet pack $UILinuxX64ProjectPath -c $Configuration -o ../../build --include-symbols
 	
 export UIOsxX64ProjectPath=./src/sql-d.ui.osx-x64/SqlD.UI.osx-x64.csproj
-dotnet publish $UIOsxX64ProjectPath -r osx-x64 --self-contained || EXIT /B 1
-cp -rf src\sql-d.ui.osx-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.osx-x64\bin\Release\netcoreapp2.2\osx-x64\publish\Views\
-dotnet pack $UIOsxX64ProjectPath -o ../../build --include-symbols || EXIT /B 1
+dotnet publish $UIOsxX64ProjectPath -c $Configuration -r osx-x64 -f $TargetFramework --self-contained 
+cp -rf "src/sql-d.ui.osx-x64/bin/Release/${TargetFramework}/osx-x64/Views" "src/sql-d.ui.osx-x64/bin/Release/${TargetFramework}/osx-x64/publish/Views/"
+dotnet pack $UIOsxX64ProjectPath -c $Configuration -o ../../build --include-symbols 
 
 export UIWinX64ProjectPath=./src/sql-d.ui.win-x64/SqlD.UI.win-x64.csproj
-dotnet publish $UIWinX64ProjectPath -r win-x64 --self-contained || EXIT /B 1
-cp -rf src\sql-d.ui.win-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui.win-x64\bin\Release\netcoreapp2.2\win-x64\publish\Views\
-dotnet pack $UIWinX64ProjectPath -o ../../build --include-symbols || EXIT /B 1
+dotnet publish $UIWinX64ProjectPath -c $Configuration -r win-x64 -f $TargetFramework --self-contained 
+cp -rf "src/sql-d.ui.win-x64/bin/Release/${TargetFramework}/win-x64/Views" "src/sql-d.ui.win-x64/bin/Release/${TargetFramework}/win-x64/publish/Views/"
+dotnet pack $UIWinX64ProjectPath -c $Configuration -o ../../build --include-symbols 
