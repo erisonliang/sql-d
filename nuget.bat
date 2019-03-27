aa78d0d64beb926b6709776666e02bc72a35d0bf
RealOrko <realorko@nowhere.com>
2019-03-27 20:36:07 +0000

    Published UI nugets

--- nuget.bat
+++ nuget.bat
@@ -15,3 +15,9 @@ dotnet nuget push ./build/sql-d/sql-d.start.linux-x64.%VERSION%.nupkg -s https:/
 dotnet nuget push ./build/sql-d/sql-d.start.osx-x64.%VERSION%.nupkg -s https://api.nuget.org/v3/index.json -k %NUGET_API_PUBLISH_KEY%
 
 dotnet nuget push ./build/sql-d/sql-d.start.linu-x64.%VERSION%.nupkg -s https://api.nuget.org/v3/index.json -k %NUGET_API_PUBLISH_KEY%
+
+dotnet nuget push ./build/sql-d/sql-d.ui.linux-x64.%VERSION%.nupkg -s https://api.nuget.org/v3/index.json -k %NUGET_API_PUBLISH_KEY%
+
+dotnet nuget push ./build/sql-d/sql-d.ui.osx-x64.%VERSION%.nupkg -s https://api.nuget.org/v3/index.json -k %NUGET_API_PUBLISH_KEY%
+
+dotnet nuget push ./build/sql-d/sql-d.ui.linu-x64.%VERSION%.nupkg -s https://api.nuget.org/v3/index.json -k %NUGET_API_PUBLISH_KEY%
--- nuget.sh
+++ nuget.sh
@@ -11,3 +11,9 @@ dotnet nuget push "./build/sql-d.start.linux-x64.$VERSION.nupkg" -s https://api.
 dotnet nuget push "./build/sql-d.start.osx-x64.$VERSION.nupkg" -s https://api.nuget.org/v3/index.json -k $NUGET_API_PUBLISH_KEY --timeout 9999
 
 dotnet nuget push "./build/sql-d.start.win-x64.$VERSION.nupkg" -s https://api.nuget.org/v3/index.json -k $NUGET_API_PUBLISH_KEY --timeout 9999
+
+dotnet nuget push "./build/sql-d.ui.linux-x64.$VERSION.nupkg" -s https://api.nuget.org/v3/index.json -k $NUGET_API_PUBLISH_KEY --timeout 9999
+
+dotnet nuget push "./build/sql-d.ui.osx-x64.$VERSION.nupkg" -s https://api.nuget.org/v3/index.json -k $NUGET_API_PUBLISH_KEY --timeout 9999
+
+dotnet nuget push "./build/sql-d.ui.win-x64.$VERSION.nupkg" -s https://api.nuget.org/v3/index.json -k $NUGET_API_PUBLISH_KEY --timeout 9999
