5c4c423af8de8b90a1dc22ca86e07d3f676fc1f8
RealOrko <RealOrko@nowhere.com>
2019-04-05 16:27:32 +0100

    Created way of installing sql-d.ui on win-x64

--- install.win.bat
+++ install.win.bat
@@ -18,8 +18,20 @@ if "!path:%PATH_TO_INSERT%=!" equ "%path%" (
    setx PATH "%PATH_TO_INSERT%;%PATH%"
 )
 
-echo "[SQL-D]: Please restart your console so that the environment variables can take effect"
-echo "[SQL-D]: Please type 'sql-d -w' to launch and browse to http://localhost:5000/swagger"
+dotnet tool install --global dotnet-cli-zip
+powershell -Command "Invoke-WebRequest https://www.nuget.org/api/v2/package/sql-d.ui.win-x64/1.0.5 -OutFile build\sql-d.ui.win-x64.nupkg"
+z -e ./build/sql-d.ui.win-x64.nupkg ./build/sql-d.ui.win-x64/
+rm -rf %userprofile%\.sql-d.ui\
+mkdir %userprofile%\.sql-d.ui\
+xcopy .\build\sql-d.ui.win-x64\contentFiles\any\any\sql-d.ui\win-x64\* %userprofile%\.sql-d.ui\ /s /e
+copy %userprofile%\.sql-d.ui\SqlD.UI.win-x64.exe %userprofile%\.sql-d.ui\sql-d.ui.exe 
+set "PATH_TO_INSERT=%userprofile%\.sql-d.ui\"
+if "!path:%PATH_TO_INSERT%=!" equ "%path%" (
+   setx PATH "%PATH_TO_INSERT%;%PATH%"
+)
+
+echo "[SQL-D]: Type 'sql-d.ui' to launch ui and browse to http://localhost:5000/"
+echo "[SQL-D]: Type 'sql-d -w' to launch and browse to http://localhost:5000/swagger"
 echo "[SQL-D]: For more sql-d options, please type 'sql-d' and hit enter"
 echo "[SQL-D]: Example ~> sql-d -n newservice1 -s "localhost:9000" -d "newservice1.db" -r "localhost:9000" -w"
-
+echo "[SQL-D]: Please restart your console so that the environment variables can take effect"
