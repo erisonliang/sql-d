046dd5a11ca44f2e6c1e01c978b3099c7912a0a8
RealOrko <RealOrko@nowhere.com>
2019-04-19 14:45:59 +0100

    Got environment updates to paths working on windows, turns out you really cannot do this through dos anymore, so powershell is required

---
+++ env-path.ps1
@@ -0,0 +1,14 @@
+﻿$envPaths = [Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)  -split ";"; 
+ns$envPaths = $envPaths | select -unique | ? {$_} 
+
+if ($envPaths -notcontains "%userprofile%\.sql-d") { 
+	$envPaths = , "%userprofile%\.sql-d" + $envPaths
+}
+
+if ($envPaths -notcontains "%userprofile%\.sql-d.ui") { 
+	$envPaths = , "%userprofile%\.sql-d.ui" + $envPaths
+}
+
+$joinedEnvPaths = [string]::Join(";", $envPaths)
+Write-Host "Setting path environment variable to $joinedEnvPaths"
+[Environment]::SetEnvironmentVariable("PATH", $joinedEnvPaths, [System.EnvironmentVariableTarget]::User) 
--- install.win.bat
+++ install.win.bat
@@ -15,10 +15,6 @@ rm -rf %userprofile%\.sql-d\
 mkdir %userprofile%\.sql-d\
 xcopy .\build\sql-d.start.win-x64\contentFiles\any\any\sql-d.start\win-x64\* %userprofile%\.sql-d\ /s /e
 copy %userprofile%\.sql-d\SqlD.Start.win-x64.exe %userprofile%\.sql-d\sql-d.exe 
-set "PATH_TO_INSERT=%userprofile%\.sql-d\"
-if "!path:%PATH_TO_INSERT%=!" equ "%path%" (
-   setx PATH "%PATH_TO_INSERT%;%PATH%"
-)
 
 dotnet tool install --global dotnet-cli-zip
 powershell -Command "Invoke-WebRequest https://www.nuget.org/api/v2/package/sql-d.ui.win-x64/1.0.6 -OutFile build\sql-d.ui.win-x64.nupkg"
@@ -27,10 +23,8 @@ rm -rf %userprofile%\.sql-d.ui\
 mkdir %userprofile%\.sql-d.ui\
 xcopy .\build\sql-d.ui.win-x64\contentFiles\any\any\sql-d.ui\win-x64\* %userprofile%\.sql-d.ui\ /s /e
 copy %userprofile%\.sql-d.ui\SqlD.UI.win-x64.exe %userprofile%\.sql-d.ui\sql-d.ui.exe 
-set "PATH_TO_INSERT=%userprofile%\.sql-d.ui\"
-if "!path:%PATH_TO_INSERT%=!" equ "%path%" (
-   setx PATH "%PATH_TO_INSERT%;%PATH%"
-)
+
+powershell -file .\env-path.ps1 
 
 echo "[SQL-D]: Type 'sql-d.ui' to launch ui and browse to http://localhost:5000/"
 echo "[SQL-D]: Type 'sql-d -w' to launch and browse to http://localhost:5000/swagger"
