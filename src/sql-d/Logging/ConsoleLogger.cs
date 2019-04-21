4119c86fc77477485c789bc613aa60b256bc098a
RealOrko <RealOrko@nowhere.com>
2019-04-21 13:57:38 +0100

    Refactored console logger, also update depndencies for nuspec file, these were out of date with the project itself

--- src/sql-d/Logging/ConsoleLogger.cs
+++ src/sql-d/Logging/ConsoleLogger.cs
@@ -6,34 +6,34 @@ namespace SqlD.Logging
 	{
 		public void Debug(string message)
 		{
-			Console.WriteLine($"SqlD: Debug: {message}");
+            Console.WriteLine($"SqlD: Debug: {message}");
 		}
 
 		public void Info(string message)
 		{
-			Console.WriteLine($"SqlD: Info: {message}");
+            Console.WriteLine($"SqlD: Info: {message}");
 		}
 
 		public void InfoInline(string message)
 		{
 			var msg = $"SqlD: Info: {message}";
-			Console.Write(msg);
-			Console.SetCursorPosition(Console.CursorLeft - msg.Length, Console.CursorTop);
+            Console.Write(msg);
+            Console.SetCursorPosition(Console.CursorLeft - msg.Length, Console.CursorTop);
 		}
 
 		public void Warn(string message)
 		{
-			Console.WriteLine($"SqlD: Warn: {message}");
+            Console.WriteLine($"SqlD: Warn: {message}");
 		}
 
 		public void Error(string message)
 		{
-			Console.WriteLine($"SqlD: Error: {message}");
+            Console.WriteLine($"SqlD: Error: {message}");
 		}
 
 		public void Fatal(string message)
 		{
-			Console.WriteLine($"SqlD: Fatal: {message}");
+            Console.WriteLine($"SqlD: Fatal: {message}");
 		}
 	}
 }
