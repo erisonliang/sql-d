6399d2ac8637926543532620ff4de45a3ee8c29d
RealOrko <realorko@nowhere.com>
2019-12-11 19:10:59 +0000

    Fixing windows build to include .\build package source

--- build.bat
+++ build.bat
@@ -15,5 +15,5 @@ dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d --source %CD%\build --p
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d.start.win-x64 --source %CD%\build --package-directory .\build
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d.start.linux-x64 --source %CD%\build --package-directory .\build
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d.start.osx-x64 --source %CD%\build --package-directory .\build
-dotnet build .\tests\sql-d\SqlD.Tests.csproj -r win-x64 -c Debug
+dotnet build .\tests\sql-d\SqlD.Tests.csproj -r win-x64 -c Debug --source $(pwd)/build
 dotnet test .\tests\sql-d\SqlD.Tests.csproj -r win-x64 -c Debug
