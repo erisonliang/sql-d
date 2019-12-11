3ff272d216a4c05be2d2c1e14d8055df173ddbeb
RealOrko <realorko@nowhere.com>
2019-12-11 19:47:35 +0000

    Looks like consistency between linux-x64 and win-x64 are very divergent!

--- build.bat
+++ build.bat
@@ -10,9 +10,12 @@ SET Configuration=Release
 SET TargetFramework=netcoreapp3.1
 
 dotnet restore .\tests\sql-d\SqlD.Tests.csproj
+dotnet nuget locals global-packages --clear
+
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d --source %CD%\build --package-directory .\build
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d.start.win-x64 --source %CD%\build --package-directory .\build
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d.start.linux-x64 --source %CD%\build --package-directory .\build
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d.start.osx-x64 --source %CD%\build --package-directory .\build
+
 dotnet build .\tests\sql-d\SqlD.Tests.csproj -r win-x64 -c Debug --source %CD%\build
 dotnet test .\tests\sql-d\SqlD.Tests.csproj -r win-x64 -c Debug
