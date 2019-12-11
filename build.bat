2edec9344f0888f56ee210c826f1ed23216215fd
RealOrko <realorko@nowhere.com>
2019-12-11 19:38:46 +0000

    Adding pre-emptive restore for win-x64?

--- build.bat
+++ build.bat
@@ -9,6 +9,7 @@ ECHO "Please make sure you call `publish` first. This will fail otherwise."
 SET Configuration=Release
 SET TargetFramework=netcoreapp3.1
 
+dotnet restore .\tests\sql-d\SqlD.Tests.csproj
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d --source %CD%\build --package-directory .\build
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d.start.win-x64 --source %CD%\build --package-directory .\build
 dotnet add .\tests\sql-d\SqlD.Tests.csproj package sql-d.start.linux-x64 --source %CD%\build --package-directory .\build
